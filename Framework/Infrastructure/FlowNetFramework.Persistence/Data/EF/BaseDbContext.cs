33856801b6a289d5382204b1eeaaed507fb1e0cc
Burak Coşkun <burakc34@hotmail.com>
2025-11-03 02:48:00 +0300

    .

--- Framework/Infrastructure/FlowNetFramework.Persistence/Data/EF/BaseDbContext.cs
+++ Framework/Infrastructure/FlowNetFramework.Persistence/Data/EF/BaseDbContext.cs
@@ -7,19 +7,6 @@ namespace FlowNetFramework.Persistence.Data.EF
 {
     public abstract class BaseDbContext : DbContext
     {
-        private readonly IHttpContextAccessor httpContextAccessor;
-
-        public BaseDbContext(DbContextOptions options, IHttpContextAccessor httpContextAccessor) : base(options)
-        {
-            this.httpContextAccessor = httpContextAccessor;
-        }
-        //
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.AddInterceptors(new SaveAuditInterceptor(httpContextAccessor));
-            base.OnConfiguring(optionsBuilder);
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             foreach (var entityType in modelBuilder.Model.GetEntityTypes())
